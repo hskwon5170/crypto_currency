8942d4612bbbd24e673a81b9fc3b317efae081e9
Hyung Seok Kwon <hskwon517@gmail.com>
2023-10-25 13:16:10 +0900

    style : tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,53 +4,24 @@ module.exports = {
   important: true,
   theme: {
     extend: {
-      borderRadius: {
-        appS: "10px",
-        appM: "20px",
-        appL: "30px",
+      backgroundColor: {
+        primary: "#DDE9F5",
+        coin: "#ffffff",
+        card: "#dbe4ff",
+        btn: "#f1f3f5",
       },
-      fontSize: {
-        title: ["2.375rem", "3.325rem"],
-        lg: ["1.125rem", "140%"],
+      textColor: {
+        primary: "#04091a",
+        sub: "#04091a80",
+        card: "#04091a",
       },
-      fontFamily: {
-        mono: ["nanumsquare"],
+      borderColor: {
+        theme: "#035FE8",
+        accent: "#2354e6",
       },
-      fontWeight: {
-        default: "400",
-        bold: "700",
-      },
-      boxShadow: {
-        frame: "1px 1px 4px 0px rgba(0, 0, 0, 0.1)",
-      },
-      spacing: {
-        ten: "0.625rem", // 10px,
-        ten2: "1.25rem", // 20px,
-        main: "1.875rem", // 30px
-        main2: "3.75rem", // 60px
-        header: "3.125rem", // 50px
-        12: "3.125rem", // ...
-        45: "11.25rem", // ...
-        36: "8.75rem", // ...
-        ch: "calc(100vh - 14vh)",
-        authw: "62.5%",
-      },
-      colors: {
-        primary: "#007DF9",
-        smooth: "#6b7c99",
-        middle: "#e3e9f3",
-        opacity: "#F5F8FF",
-        warning: "#ffcc00",
-        highlight: "#007dfe",
-        pencil: "#333333",
-        grayop: "#E4E7EA",
-        "gray-1": "#A8ABAE",
-        "gray-2": "#808386",
-      },
-      screens: {
-        sm: { max: "767px" },
-        md: { min: "768px", max: "991px" },
-        lg: { min: "1080px" },
+      gradientColorStops: {
+        theme: "#035FE8",
+        accent: "#2354e6",
       },
     },
   },
