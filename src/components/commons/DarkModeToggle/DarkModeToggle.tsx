17da6f8a0db8949d03f34ed4cce322eadfef352d
Hyung Seok Kwon <hskwon517@gmail.com>
2023-11-30 12:08:19 +0900

    feat: 다크모드 토글 트랜지션 추가

--- src/components/commons/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/commons/DarkModeToggle/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useDarkModeStore } from "../ZustandStore/ZustandStore";
 import { BsFillSunFill } from "react-icons/bs";
 import { BsFillMoonFill } from "react-icons/bs";
@@ -9,18 +9,25 @@ interface DarkModeToggleProps {
 
 export const DarkModeToggle: FC<DarkModeToggleProps> = ({ className }) => {
   const { dark, toggleDarkMode } = useDarkModeStore();
+  const [isToggle, setIsToggle] = useState<boolean>(false);
+  const onClickToggle = () => {
+    toggleDarkMode();
+    setIsToggle((prev) => !prev);
+  };
 
   return (
     <div
-      className={`${className}  ${
-        dark
-          ? "text-white bg-gray-800 transform rotate-[360deg] ease-in-out transition-all duration-300"
-          : "text-black bg-gray-200"
-      } w-8 h-8 rounded-xl flex items-center justify-center shadow-2xl `}
+      className={`rounded-md w-[50px] absolute right-10 cursor-pointer bg-[#f3f4f6] shadow-2xl`}
+      onClick={onClickToggle}
     >
-      <button onClick={toggleDarkMode}>
-        {!dark ? <BsFillMoonFill /> : <BsFillSunFill />}
-      </button>
+      <div
+        className={`${className} 
+      ${isToggle ? "-translate-x-full" : "translate-x-0"}  ${
+          dark ? " transform ease-in-out transition-all" : ""
+        } text-white bg-black relative left-6  transition-transform duration-75 w-8 h-8 rounded-xl flex items-center justify-center shadow-2xl `}
+      >
+        <button>{!dark ? <BsFillMoonFill /> : <BsFillSunFill />}</button>
+      </div>
     </div>
   );
 };
