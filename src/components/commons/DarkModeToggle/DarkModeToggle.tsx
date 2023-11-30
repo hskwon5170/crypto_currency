854b07bdcdaf38b6625669047ae9096389000cf2
Hyung Seok Kwon <hskwon517@gmail.com>
2023-11-30 13:22:18 +0900

    fix: 다크모드 토글 버튼 스타일 수정

--- src/components/commons/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/commons/DarkModeToggle/DarkModeToggle.tsx
@@ -17,14 +17,18 @@ export const DarkModeToggle: FC<DarkModeToggleProps> = ({ className }) => {
 
   return (
     <div
-      className={`rounded-md w-[50px] absolute right-10 cursor-pointer bg-[#f3f4f6] shadow-2xl`}
+      className={`rounded-3xl w-[68px] h-[40px] absolute right-10 cursor-pointer ${
+        dark ? "bg-[#7a1f50]" : "bg-gray-200"
+      } shadow-2xl`}
       onClick={onClickToggle}
     >
       <div
         className={`${className} 
-      ${isToggle ? "-translate-x-full" : "translate-x-0"}  ${
-          dark ? " transform ease-in-out transition-all" : ""
-        } text-white bg-black relative left-6  transition-transform duration-75 w-8 h-8 rounded-xl flex items-center justify-center shadow-2xl `}
+      ${isToggle ? "-translate-x-full" : "translate-x-[-8px]"}  ${
+          dark
+            ? " transform ease-in-out transition-all rotate-180"
+            : "rotate-[-360deg]"
+        } text-white bg-black relative left-[40px] top-[4px] transition-transform duration-300 w-8 h-8 rounded-3xl flex items-center justify-center shadow-2xl`}
       >
         <button>{!dark ? <BsFillMoonFill /> : <BsFillSunFill />}</button>
       </div>
