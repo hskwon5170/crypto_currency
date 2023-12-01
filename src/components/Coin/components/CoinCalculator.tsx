2bb4cd8929356101d1e3981da16adddd23a6c369
Hyung Seok Kwon <hskwon517@gmail.com>
2023-12-01 14:38:30 +0900

    fix: Calculator bg 스타일 수정

--- src/components/Coin/components/CoinCalculator.tsx
+++ src/components/Coin/components/CoinCalculator.tsx
@@ -3,6 +3,7 @@ import { InputArea } from "./InputArea";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { CoinDetailResponse } from "../types";
 import { useDarkModeStore } from "../../commons/ZustandStore/ZustandStore";
+import "./CoinCalculator.css";
 
 interface DataProps {
   data: CoinDetailResponse;
@@ -27,33 +28,35 @@ export const CoinCalculator = ({
         dark
           ? "bg-black border-[#2C2C2C] shadow-gray-600 shadow-2xl"
           : "bg-white border-[#f2f2f2] shadow-2xl"
-      } border-[2px]   rounded-3xl max-w-[30rem] max-h-[30rem] p-2 gap-10 relative sm:w-[95%]`}
+      } border-[2px]   rounded-3xl max-w-[30rem] max-h-[30rem] p-2 gap-10 relative sm:w-[95%] wrap`}
     >
-      <div className="font-semibold pl-3 py-3">Swap</div>
-      <div className="flex flex-col items-center space-y-6 relative">
-        <InputArea
-          subTitle="You pay"
-          isImage
-          imgUrl={data.image.large}
-          symbol={data.symbol}
-          onChangeToken={onChangeToken}
-        />
-        <div
-          className={`absolute top-[calc(50%-20px)] transform -translate-y-1/2 ${
-            dark ? "bg-black text-white " : "bg-white text-black"
-          } p-3 rounded-xl`}
-        >
-          <div className={` ${dark ? "bg-[#2C2C2C]" : "bg-[#f9f9f9]"} p-2`}>
-            <AiOutlineArrowDown />
+      <div className={`${dark ? "bg-black" : "bg-white"} h-full rounded-3xl`}>
+        <div className="font-semibold pl-3 py-3">Swap</div>
+        <div className="flex flex-col items-center space-y-6 relative">
+          <InputArea
+            subTitle="You pay"
+            isImage
+            imgUrl={data.image.large}
+            symbol={data.symbol}
+            onChangeToken={onChangeToken}
+          />
+          <div
+            className={`absolute top-[calc(50%-20px)] transform -translate-y-1/2 ${
+              dark ? "bg-black text-white " : "bg-white text-black"
+            } p-3 rounded-xl`}
+          >
+            <div className={` ${dark ? "bg-[#2C2C2C]" : "bg-[#f9f9f9]"} p-2`}>
+              <AiOutlineArrowDown />
+            </div>
           </div>
+          <InputArea
+            subTitle={`You receive (${currency?.toUpperCase()})`}
+            calculatedUSD={calculatedUSD}
+            globalCurrency
+            onChangeCurrency={onChangeCurrency}
+            currency={currency}
+          />
         </div>
-        <InputArea
-          subTitle={`You receive (${currency?.toUpperCase()})`}
-          calculatedUSD={calculatedUSD}
-          globalCurrency
-          onChangeCurrency={onChangeCurrency}
-          currency={currency}
-        />
       </div>
     </div>
   );
